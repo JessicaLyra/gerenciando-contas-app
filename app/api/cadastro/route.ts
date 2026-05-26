c1e7756f28474d7a5a6caf192ae167b4b83044a9
JessicaLyra <jessica.lyra@outlook.com>
2026-05-26 17:26:10 -0300

    test: valida execução da API de cadastro na Vercel

--- app/api/cadastro/route.ts
+++ app/api/cadastro/route.ts
@@ -1,51 +1,9 @@
 import { NextResponse } from "next/server";
-import bcrypt from "bcrypt";
-import { prisma } from "@/lib/prisma";
 
+export async function POST() {
+  console.log("🔥 API CADASTRO FUNCIONOU");
 
-export async function POST(request: Request) {
-  console.log("🔥 CADASTRO CHAMADO");
-  console.log("DATABASE_URL:", process.env.DATABASE_URL);
-
-  try {
-    const body = await request.json();
-
-    const { usuario, email, senha } = body;
-
-    // verifica usuário existente
-    const usuarioExistente = await prisma.user.findUnique({
-      where: { email },
-    });
-
-    if (usuarioExistente) {
-      return NextResponse.json(
-        { message: "Este e-mail já está cadastrado" },
-        { status: 400 }
-      );
-    }
-
-    // hash senha
-    const senhaCriptografada = await bcrypt.hash(senha, 10);
-
-    // cria usuário
-    await prisma.user.create({
-      data: {
-        usuario,
-        email,
-        senha: senhaCriptografada,
-      },
-    });
-
-    return NextResponse.json({
-      message: "Cadastro realizado com sucesso",
-    });
-
-  } catch (error) {
-    console.error("ERRO REAL:", error);
-
-    return NextResponse.json(
-      { message: "Erro interno no servidor" },
-      { status: 500 }
-    );
-  }
+  return NextResponse.json({
+    message: "API funcionando",
+  });
 }
